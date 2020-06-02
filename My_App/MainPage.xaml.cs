cc5bd575c3bd77c157b1ee6fa771e87812ee3d16
msdnwptest <54531691+msdnwptest@users.noreply.github.com>
2020-06-02 21:28:03 +0800

    Add project files.

---
+++ My_App/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using My_App.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace My_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<ListItem> listItems;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            listItems = ItemManager.GetListItems();
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ My_App/Models/ListItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_App.Models
+{
+    public class ListItem
+    {
+        public string BookTitle { get; set; }
+    }
+
+    public class ItemManager
+    {
+        public static List<ListItem> GetListItems()
+        {
+            var items = new List<ListItem>
+            {
+                new ListItem { BookTitle = "The Wizard of Oz" }
+            };
+
+            return items;
+        }
+    }
+}
